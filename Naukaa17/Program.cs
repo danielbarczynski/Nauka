21732de561da07d7ed76a7a82cda909cd98a306a
WhatIveBecome <daniel.barczynski@onet.pl>
2021-10-17 13:38:46 +0200

    Added Naukaa17

---
+++ Naukaa17/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Naukaa17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Queue<string> animals = new Queue<string>();
+            animals.Enqueue("cat"); // enqueue mens add to queue
+            animals.Enqueue("dog");
+            animals.Enqueue("bird");
+            animals.Enqueue("hamster");
+
+            foreach (string s in animals)
+                Console.WriteLine(s);
+
+        }
+    }
+}
