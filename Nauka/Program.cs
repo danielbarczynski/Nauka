c3f5780c999ab09372804110a2a54f5a0fb8c4e6
WhatIveBecome <daniel.barczynski@onet.pl>
2021-10-17 15:56:29 +0200

    Practicing on Nauka0

--- Nauka/Program.cs
+++ Nauka/Program.cs
@@ -7,10 +7,11 @@ namespace Nauka
         static void Main(string[] args)
         {
             Random legi = new Random();
+
             int x = legi.Next(0, 2000);
             int y = legi.Next(0, 2000);
-            
-            Console.WriteLine("x={0}, y={1}", x, y);
+
+            Console.WriteLine("x: {0}" + "\ny: {1}", x, y);
 
             if (x < y)
             {
@@ -30,14 +31,16 @@ namespace Nauka
             }
 
             int m = 10;
-            int n = 5;
+            int n = legi.Next(0,20);
+
+            Console.WriteLine("\nm: {0}" + "\nn: {1}", m, n);
 
             bool w = m > n;
 
-            if (m < n)
-                Console.WriteLine("m bigger");
+            if (w)
+                Console.WriteLine("m większe");
             else
-                Console.WriteLine("m not bigger");
+                Console.WriteLine("\nn większe");
        
 
 
