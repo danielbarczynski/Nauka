7ac1b2e1636114b506710762d4ed49e8c49ed592
WhatIveBecome <daniel.barczynski@onet.pl>
2021-10-17 17:29:00 +0200

    finishing nauka3

--- Nauka3/Program.cs
+++ Nauka3/Program.cs
@@ -7,20 +7,20 @@ namespace Nauka3
         static void Main(string[] args)
         {
             Random rnd = new Random();
-            int a, printNumber = 0;
+            int a, b , i = 0;
 
-            while (printNumber < 51)   // while (end)
+            while (i < 51)   // while (end)
             {
                 a = rnd.Next(0, 100);
                 if (a % 2 == 0) // czy podzielna przez 2, czy wychodzi jej 0 reszty, jesli tak, zapisz
                 {
-                    printNumber++;
-                    Console.WriteLine("number {0}: (even) {1}", printNumber, a); // printnumber from a
+                    i++; // same as for (int i = 0) loop
+                    Console.WriteLine("number {0}: (even) {1}", i, a); // printnumber from a
                 }
                 if (a % 3 == 0)
                 {
-                    printNumber++;
-                    Console.WriteLine("number {0}: (odd) {1}", printNumber, a);
+                    i++;
+                    Console.WriteLine("number {0}: (odd) {1}", i, a);
                 }
                 else
                     continue;
@@ -28,14 +28,14 @@ namespace Nauka3
             }
 
             //----------------------------------------------------------------------------------------
-            int b, i = 0; // printnumber is only name, in this example I have inserted c instead
+           
 
             do // NAJPIERW ROBI, POTEM SPRAWDZA CZY MA RO BIĆ DALEJ
             { 
              b = rnd.Next(0, 10);
                 if (b % 2 == 0) // czy podzielna przez 2, czy wychodzi jej 0 reszty, jesli tak, zapisz
                 {
-                    i++; // same as for (int i = 0) loop
+                    i++; 
                     Console.WriteLine("number {0}: {1}", i, b);
                 }
                 else  
