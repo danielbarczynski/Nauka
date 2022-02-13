2a960f512cc8c482378243bb0aee673326e38038
Daniel <daniel.barczynski@onet.pl>
2022-02-13 14:51:02 +0100

    update

--- Naukaa62(mediator)/Program62.cs
+++ Naukaa62(mediator)/Program62.cs
@@ -1,10 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 
+public interface IUzytkownik
+{
+    void WyslijWiadomosc(string wiadomosc);
+    void OdbierzWiadomosc(string wiadomosc);
+}
 
-//
-//
-//
+public interface IMediator
+{
+    void DodajUzytkownika(IUzytkownik uzytkownik);
+    void WyslijWiadomosc(string wiadomosc, IUzytkownik nadawca);
+}
 
 public class Mediator : IMediator
 {
@@ -17,21 +24,36 @@ public class Mediator : IMediator
 
     public void DodajUzytkownika(IUzytkownik uzytkownik)
     {
-        //
-        //
+        uzytkownicy.Add(uzytkownik);
     }
 
     public void WyslijWiadomosc(string wiadomosc, IUzytkownik nadawca)
     {
-        //
-        //
-        //
+        nadawca.OdbierzWiadomosc(wiadomosc);
     }
 }
 
-//
-//
-//
+public class Klient : IUzytkownik
+{
+    string login;
+    IMediator Mediator;
+
+    public Klient(IMediator Mediator, string login)
+    {
+        this.login = login;
+        this.Mediator = Mediator;
+    }
+
+    public void WyslijWiadomosc(string wiadomosc)
+    {
+        Mediator.WyslijWiadomosc(wiadomosc, this);
+    }
+
+    public void OdbierzWiadomosc(string wiadomosc)
+    {
+        Console.WriteLine("Użytkownik " + login + " otrzymal wiadomosc: " + wiadomosc);
+    }
+}
 
 public class Dev : IUzytkownik
 {
@@ -55,27 +77,25 @@ public class Dev : IUzytkownik
     }
 }
 
-//
-//
-//
-
 class Program62
 {
     static void Main(string[] args)
     {
 
-        //
+        Mediator mediator = new Mediator();
 
         IUzytkownik ania = new Klient(mediator, "Ania");
-        //
-        //
+        IUzytkownik geohot = new Dev(mediator, "Geohot");
+        IUzytkownik nakamoto = new Dev(mediator, "Nakamoto");
 
-        //
+        mediator.DodajUzytkownika(ania);
+        mediator.DodajUzytkownika(geohot);
         mediator.DodajUzytkownika(nakamoto);
-        //
 
-        //
-        geohot.WyslijWiadomosc("Czekam az Nakamoto zaparzy kawe...");
+        nakamoto.WyslijWiadomosc("Prosze natychmiast wprowadzic poprawki na produkcje.");
+        geohot.WyslijWiadomosc("Prosze natychmiast wprowadzic poprawki na produkcje.");
+        ania.WyslijWiadomosc("Czekam az Nakamoto zaparzy kawe...");
+        nakamoto.WyslijWiadomosc("Czekam az Nakamoto zaparzy kawe...");
 
     }
 }
