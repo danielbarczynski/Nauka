2928df409f6ff27e22cf62cabff9abb6ca400c72
Daniel <daniel.barczynski@onet.pl>
2022-04-13 11:40:05 +0200

    update

---
+++ Naukaa77(func)/Program77.cs
@@ -0,0 +1,44 @@
+﻿// C# program to illustrate Func delegate
+using System;
+
+class GFG
+{
+
+    // for func
+    public static int method(string num)
+    {
+        return 5;
+    }
+
+    public static bool IsTrue(int n)
+    {
+        if (n == 0)
+            return true;
+        return false;
+    }
+
+    // for action
+
+    public static void Actionn(bool isBool)
+    {
+        bool isBooll = isBool;
+        Console.WriteLine(isBooll);
+    }
+
+    static public void Main()
+    {
+        Func<string, int> myfun = method; // (string (in)) & return int (out)
+
+        Console.WriteLine(myfun("h"));
+
+        //Console.WriteLine(method("h"));
+
+        Func<int, bool> boolFunc = IsTrue;
+        Console.WriteLine(boolFunc(0));
+
+        // można również używać z anonimowymi funkcjami
+
+        Action<bool> actionFunc = Actionn; // tylko jeden <T> argument, bo domyślnie jest void
+        Actionn(false);
+    }
+}
