c584670c011fad397e973aa80bce15db20e56092
Daniel <daniel.barczynski@onet.pl>
2022-04-13 18:22:40 +0200

    update

---
+++ Naukaa78(json)/Program78.cs
@@ -0,0 +1,41 @@
+﻿using System.Text.Json;
+
+namespace SerializeToFile
+{
+    public class WeatherForecast
+    {
+        public DateTimeOffset Date { get; set; }
+        public int TemperatureCelsius { get; set; }
+        public string? Summary { get; set; }
+    }
+
+    public class Program78
+    {
+        public static void Main()
+        {
+            var weatherForecast = new WeatherForecast
+            {
+                Date = DateTime.Parse("2019-08-01"),
+                TemperatureCelsius = 25,
+                Summary = "Hot"
+            };
+
+            string fileName = "WeatherForecast.json";
+            string jsonString = JsonSerializer.Serialize(weatherForecast);
+            File.WriteAllText(fileName, jsonString); // zapisanie jsona do pliku
+            string jsonString2 = File.ReadAllText(fileName);
+            Console.WriteLine(jsonString);
+            Console.WriteLine(jsonString2); // odczytanie pliku
+
+            // deserialization
+            // 
+            weatherForecast = JsonSerializer.Deserialize<WeatherForecast>(jsonString2)!;
+
+            Console.WriteLine($"Date: {weatherForecast.Date}");
+            Console.WriteLine($"TemperatureCelsius: {weatherForecast.TemperatureCelsius}");
+            Console.WriteLine($"Summary: {weatherForecast.Summary}");
+        }
+    }
+}
+// output:
+//{"Date":"2019-08-01T00:00:00-07:00","TemperatureCelsius":25,"Summary":"Hot"}
---
+++ Naukaa79(files)/Program79.cs
@@ -0,0 +1,49 @@
+﻿DirectoryInfo dir = new DirectoryInfo("."); // current directory
+Console.WriteLine(dir.FullName);
+Console.WriteLine(dir.CreationTime);
+
+//string filename1 = @"c:\documents\files\u0066.txt";
+//string filename2 = "c:\\documents\\files\\u0066.txt";
+
+string[] customers =
+{
+    "bob",
+    "mark",
+    "jay"
+};
+
+string filePath = @"C:\Users\itsmy\test";
+File.WriteAllLines(filePath, customers);
+
+var read = File.ReadAllLines(filePath); 
+
+foreach (string line in read)
+{
+    Console.WriteLine(line);
+}
+
+DirectoryInfo dir2 = new DirectoryInfo(@"C:\Users\itsmy\source\repos\WhatIveBecome\Nauka");
+FileInfo[] txtFiles = dir2.GetFiles("*.txt", SearchOption.AllDirectories); // moze byc bez drugiego argumentu, wtedy szuka tylko w tym folderze
+
+foreach (FileInfo file in txtFiles)
+{
+    Console.WriteLine(file.Name);
+    Console.WriteLine(file.Length); // in bytes
+}
+
+// ------------------------------------------------------
+Console.WriteLine();
+
+string filePath2 = @"C:\Users\itsmy\source\repos\WhatIveBecome\Nauka\Naukaa79(files)\text1";
+StreamWriter sw = File.CreateText(filePath2);
+
+sw.WriteLine("Hello there");
+sw.WriteLine("Cya!");
+sw.Close();
+
+StreamReader sr = File.OpenText(filePath2);
+
+Console.WriteLine(sr.ReadLine()); // czyta pierwsza linie
+Console.WriteLine("-------");
+Console.WriteLine(sr.ReadToEnd()); // czyta cala reszte (lub caly tekst)
+sr.Close();
