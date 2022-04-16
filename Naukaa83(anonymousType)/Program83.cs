e31d68d63557b6e28450384e8fd7d287180a6f82
Daniel <daniel.barczynski@onet.pl>
2022-04-16 14:36:31 +0200

    update

--- Naukaa83(anonymousType)/Program83.cs
+++ Naukaa83(anonymousType)/Program83.cs
@@ -1,5 +1,31 @@
 ﻿var student = new { Id = 1, FirstName = "James", LastName = "Bond" };
 Console.WriteLine($"{student.Id} {student.LastName} {student.FirstName}");
 
-student.Id = 2;//Error: cannot chage value
-student.FirstName = "Steve";//Error: cannot chage value
+//student.Id = 2;//Error: cannot chage value
+//student.FirstName = "Steve";//Error: cannot chage value
+
+//An anonymous type's property can include another anonymous type.
+
+
+var student2 = new
+{
+    Id = 1,
+    FirstName = "James",
+    LastName = "Bond",
+    Address = new { Id = 1, City = "London", Country = "UK" }
+};
+
+Console.WriteLine(student2.Address.City + "\n");
+
+// you can create array of anonymous types also
+
+var students3 = new[] {
+    new { Id = 1, FirstName = "James", LastName = "Bond" },
+    new { Id = 2, FirstName = "Steve", LastName = "Jobs" },
+    new { Id = 3, FirstName = "Bill", LastName = "Gates" }
+};
+
+foreach (var item in students3)
+{
+    Console.WriteLine(item.FirstName);
+}
---
+++ Naukaa84(init)/Program84.cs
@@ -0,0 +1,44 @@
+﻿public class Member
+{
+    public int Id // is not nullable thanks to init method
+    {
+        get;
+        init;
+    } // set is replaced with init
+    public string? Name
+    {
+        get;
+        set;
+    }
+    public string? Address
+    {
+        get;
+        set;
+    }
+}
+
+namespace Naukaa84
+{
+    class Program84
+    {
+        static void Main(string[] args)
+        {
+            Member memberObj = new Member
+            {
+                Id = 2
+            };
+
+            //memberObj.Id = 2; // only can be assigned in object initializer or this/base
+            memberObj.Name = "Kirtesh Shah";
+            memberObj.Address = "Vadodara";
+            Console.WriteLine("****************START - Member Details***********");
+            Console.WriteLine("ID : " + memberObj.Id);
+            Console.WriteLine("Name : " + memberObj.Name);
+            Console.WriteLine("Address: " + memberObj.Address);
+            Console.ReadLine();
+            Console.WriteLine("****************END - Member Details***********");
+
+            //Init-only properties can or cannot be set as per your requirement. Only ID property is set and name and address properties are not set. Please note that if any property is not set at the time of object creation that property cannot be set.
+        }
+    }
+}
