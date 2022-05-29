6cfa3f902bc89ef405722ff8e9ea6aa3e9c5c6cb
Daniel Laptop <daniel.barczynski@microsoft.wsei.edu.pl>
2022-05-29 16:06:41 +0200

    update

---
+++ Naukaa91(reflection)/Program91.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+
+// Using GetType to obtain type information:
+int i = 42;
+Type type = i.GetType();
+Console.WriteLine(type);
+
+// Using Reflection to get information of an Assembly:
+Assembly info = typeof(int).Assembly; // using system.reflection
+Console.WriteLine(info);
---
+++ Naukaa92(overloadingOperators)/Program92.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace OperatorOverloadExample
+{
+    class Program92
+    {
+        public class Rectangle
+        {
+            public int Width = 0;
+            public int Height = 0;
+
+            public Rectangle(int width, int height)
+            {
+                Width = width;
+                Height = height;
+
+            }
+            public static Rectangle operator +(Rectangle rect1, Rectangle rect2) 
+            {
+                Rectangle rectResult = new Rectangle(rect1.Width + rect2.Width, rect1.Height + rect2.Height);
+
+                return rectResult;
+
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            Rectangle rect1 = new Rectangle(10, 10);
+            Rectangle rect2 = new Rectangle(5, 10);
+
+            Rectangle rectResult = rect1 + rect2;
+            Rectangle rectResult2 = new Rectangle(rect1.Width + rect2.Width, rect1.Height + rect2.Height);
+            rectResult2 = rect1 + rect2; // works because operator ich changed to method for example "publc Rectangle Add(r1,r2) - it wont work
+            Console.WriteLine($"{rectResult.Width},{rectResult.Height}");
+            Console.WriteLine($"{rectResult2.Width},{rectResult2.Height}");
+
+            Console.ReadKey();
+        }
+    }
+}
