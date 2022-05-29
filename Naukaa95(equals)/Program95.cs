bf48afdf514315baaa31a02c4c25c29e196846d2
Daniel Laptop <daniel.barczynski@microsoft.wsei.edu.pl>
2022-05-29 18:42:06 +0200

    update

---
+++ Naukaa95(equals)/Program95.cs
@@ -0,0 +1,28 @@
+﻿string a = new string('x', 1);
+string b = new string('x', 1);
+Console.WriteLine(a == b); // Uses string's implementation, prints True
+
+object c = a;
+object d = b;
+Console.WriteLine(c == d); // Reference identity comparison, prints False
+
+string f = new string('x', 1);
+string g = new string('y', 1); // if changed to 'y', prints false
+Console.WriteLine(f.Equals((object)g));
+
+object x = f;
+object z = g;
+Console.WriteLine(x.Equals(z)); // values the same, prints true
+
+class MyClass
+{
+    private class Human // cant be in namespace
+    {
+
+    }
+}
+
+sealed class Sup // can be in namespace
+{
+
+}
