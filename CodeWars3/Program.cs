c8f97df3e1949974d6aba70fbb6ea827a05f0d3b
Daniel Laptop <daniel.barczynski@microsoft.wsei.edu.pl>
2022-06-09 01:59:27 +0200

    update

---
+++ CodeWars3/Program.cs
@@ -0,0 +1,28 @@
+﻿int num = 254342;
+Console.WriteLine(Kata.DescendingOrder(num));
+
+public static class Kata
+{
+    public static int DescendingOrder(int num)
+    {
+        string text = num.ToString();
+        var arr = text.ToArray();
+        List<char> list = new List<char>();
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            list.Add(arr[i]);           
+        }
+
+        var q1 = list.OrderByDescending(x => x);
+        q1.ToList();
+        string empty = " ";
+
+        foreach (var x in q1)
+        {
+            empty += x;
+        }
+
+        return Convert.ToInt32(empty);
+    }
+}
