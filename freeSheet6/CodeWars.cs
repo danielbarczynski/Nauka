9985ee08e0b60148c1c7cc145604072d9247e662
Daniel PC <daniel.barczynski@onet.pl>
2022-06-08 20:33:01 +0200

    update

---
+++ freeSheet6/CodeWars.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine(CustomMath.multiply(2, 3));
+string a = "QWERTyUIOP";
+string b = "QWERTUIOP";
+string c = "1";
+string d = "HELLO DONALD";
+
+Console.WriteLine(a.IsUpperCase());
+Console.WriteLine(b.IsUpperCase());
+Console.WriteLine(c.IsUpperCase());
+Console.WriteLine(d.IsUpperCase());
+
+
+public class CustomMath
+{
+    public static int multiply(int a, int b)
+    {
+        return a * b;
+    }
+}
+
+public static class StringExtensions
+{
+    public static bool IsUpperCase(this string text)
+    {
+        for (int i = 0; i < text.Length; i++)
+        {
+            if (!Char.IsUpper(text[i]) && text[i] != ' ')
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
---
+++ interview/Program.cs
@@ -0,0 +1,48 @@
+﻿
+//Napisz metodę, która posortuje powyższą listę malejąco względem wieku i zwróci ją w formie dowolnej kolekcji. Sortowanie możesz zaimplementować w jaki tylko sposób chcesz.
+
+//Definiujemy kolekcję
+List<Person> people = new List<Person>
+
+{
+
+ new Person { Name = "Jacek", Age = 4  },
+
+ new Person { Name = "Basia", Age = 12  },
+
+ new Person { Name = "Łukasz", Age = 75  },
+
+ new Person { Name = "Kasia", Age = 89  },
+
+ new Person { Name = "Zosia", Age = 33  },
+
+ new Person { Name = "Rafał", Age = 21  },
+
+ new Person { Name = "Stefan", Age = 56  },
+
+ new Person { Name = "Daniel", Age = 7  },
+
+};
+
+foreach (var item in SortowanieListy(people))
+{
+    Console.WriteLine(item.Name + item.Age);
+}
+
+List<Person> SortowanieListy(List<Person> people)
+{
+    var query1 = people.OrderByDescending(x => x.Age);
+    var query2 = query1.ToList();
+    return query2;
+}
+
+public class Person
+
+{
+    public string Name { get; set; }
+
+    public int Age { get; set; }
+
+}
+
+
