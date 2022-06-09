30226ee07d7f2ce3671f2e2171da0a0fdb8155fd
Daniel PC <daniel.barczynski@onet.pl>
2022-06-09 20:19:21 +0200

    update

---
+++ Naukaa99(concat)/Program99.cs
@@ -0,0 +1,10 @@
+﻿string s1 = "www";
+string s2 = ".readit.";
+string s3 = "pl";
+int i1 = 99;
+int[] array = { 1, 5, 9, 100 };
+
+string result = string.Concat(s1, s2, s3, i1);
+string result2 = string.Concat(array);
+Console.WriteLine(result);
+Console.WriteLine(result2);
