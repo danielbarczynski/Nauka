94ff572073a63b260165f8ee5131d4a88c3ffe7b
Daniel PC <daniel.barczynski@onet.pl>
2022-06-14 17:51:06 +0200

    update

---
+++ Naukaa100(refOut2)/Naukaaa100.cs
@@ -0,0 +1,26 @@
+﻿class Osoba
+{
+    public String imie { get; set; }
+    public Osoba(string imie) { this.imie = imie; }
+    public void Info() { Console.WriteLine("imie: {0}", imie); }
+}
+
+class Naukaaa100
+{
+    static void Main(string[] args)
+    {
+        Osoba osoba1 = new Osoba("Karol");
+        osoba1.Info(); // imie: Karol
+
+        Zmien(ref osoba1); // bez ref będzie Arek
+        osoba1.Info(); //imie: Maciek
+
+        Console.ReadKey();
+    }
+
+    public static void Zmien(ref Osoba o) // bez ref będzie Arek
+    {
+        o.imie = "Arek";         // zadziała, przy out wyjdzie błąd kompilacji
+        o = new Osoba("Maciek"); // także dzaiała!
+    }
+}
