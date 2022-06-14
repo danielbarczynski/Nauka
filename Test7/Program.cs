a7f10bab22d74059d5e5d73e90d62e890ea63da5
Daniel PC <daniel.barczynski@onet.pl>
2022-06-14 14:11:31 +0200

    update

---
+++ Test7/Program.cs
@@ -0,0 +1,33 @@
+﻿string s = "ala ma kota";
+
+Console.WriteLine(MyCLass.ReverseWordsInSentence(s));
+
+static class MyCLass
+{
+    public static string ReverseWordsInSentence(string s)
+    {
+        string result = "";
+        for (int i = s.Length; i <= 0; i--)
+        {
+            result += s[i];
+        }
+        return result;
+    }
+    public static string ReverseWordsInSentence2(string s)
+    {
+        List<string> list = new List<string>();
+
+        string[] words = s.Split(' ');
+
+        foreach (var word in words)
+        {
+            char[] charArr = word.ToCharArray();
+            Array.Reverse(charArr);
+
+            string reverse = new string(charArr);
+
+            list.Add(reverse);
+        }
+        return string.Join(" ", list.ToArray());
+    }
+}
---
+++ Test8/Program.cs
@@ -0,0 +1,39 @@
+﻿var A = new[] { 5, 6, 12, 6, 5, 5, 22 };
+Console.WriteLine(MyClass.Solve(A));
+
+static class MyClass
+{
+    //public static int Solve(int[] Array)
+    //{
+    //    Array.Distinct
+    //    return Array.GroupBy(x => x).Single(x => x.Count() == 1).Key;
+    //}
+
+    public static int Solve(int[] Array)
+    {
+        int n = 0;
+        try
+        {
+            for (int i = 0; i < Array.Length; i++)
+            {
+                if (Array[i] != Array[i + 1] && Array[i] != Array[i + 2] && Array[i] != Array[i + 3] && Array[i] != Array[i + 4])
+                {
+                    n++;
+                }
+            }
+        }
+        catch (Exception)
+        {
+            for (int i = 4; i < Array.Length; i++)
+            {
+
+                if (Array[i] != Array[i - 1] && Array[i] != Array[i - 2] && Array[i] != Array[i - 3] && Array[i] != Array[i - 4])
+                {
+                    n++;
+                }
+            }
+        }
+        return n;
+    }
+
+}
