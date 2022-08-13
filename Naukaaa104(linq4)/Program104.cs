e54a0caade26ae7f3c6c3961ba3018182ff2dd2b
Daniel Laptop <daniel.barczynski@microsoft.wsei.edu.pl>
2022-08-13 16:58:31 +0200

    update

--- Naukaaa104(linq4)/Program104.cs
+++ Naukaaa104(linq4)/Program104.cs
@@ -22,15 +22,35 @@ List<Employee> employees2 = new List<Employee>()
 var distinctCountry = countries.Distinct(); // no case sensitive
 var distinctCountry2 = countries.Distinct(StringComparer.OrdinalIgnoreCase); // one way
 var distinctEmployees = employees.Select(x => new { x.EmployeeName }).Distinct(); // third way
-var unionEmployees = employees.Select(x => new { x.EmployeeId, x.EmployeeName }).Union(employees2.Select(x=> new { x.EmployeeId, x.EmployeeName }));
+var unionEmployees = employees.Select(x => new { x.EmployeeId, x.EmployeeName }).Union(employees2.Select(x=> new { x.EmployeeId, x.EmployeeName })); // removes duplicates
+var concatEmployees = employees.Concat(employees2); // it doesn't remove duplicates
 var intersectEmployees = employees.Select(x => new { x.EmployeeId, x.EmployeeName }).Intersect(employees2.Select(x=> new { x.EmployeeId, x.EmployeeName }));
 var exceptEmployees = employees.Select(x => new { x.EmployeeId, x.EmployeeName }).Except(employees2.Select(x=> new { x.EmployeeId, x.EmployeeName }));
+var equalEmployees = employees.Select(x => new { x.EmployeeId, x.EmployeeName }).SequenceEqual(employees2.Select(x => new { x.EmployeeId, x.EmployeeName }));
+var containsEmployees = employees.Contains(new Employee { EmployeeId = 5, EmployeeName = "Magda"}); // false
+var containsEmployees2 = employees2.Select(x => new { x.EmployeeId, x.EmployeeName }).Contains(new { EmployeeId = 5, EmployeeName = "Magda" }); // true
+var allEmployees = employees.All(x => x.EmployeeId < 10); // prediacte version if all have id < 10
+var anyEmployees = employees.Any(); // if there is any element returns true
+var any2Employees = employees.Any(x => x.EmployeeId < 1); // prediacte version if any have id < 10
+var startsWithM = from e in employees
+                  where e.EmployeeName.StartsWith("M")
+                  select e;
 
-foreach (var item in exceptEmployees)
+foreach (var item in startsWithM)
+{
+    Console.WriteLine("M: " + item.EmployeeName);
+}
+
+foreach (var item in concatEmployees)
 {
     Console.WriteLine(item.EmployeeName + " " + item.EmployeeId);
 }
-
+Console.WriteLine(containsEmployees);
+Console.WriteLine(containsEmployees2);
+Console.WriteLine(equalEmployees);
+Console.WriteLine(allEmployees);
+Console.WriteLine(anyEmployees);
+Console.WriteLine(any2Employees);
 Console.WriteLine("------------------------");
 
 //-----------------------------------------------------------------------------------------
