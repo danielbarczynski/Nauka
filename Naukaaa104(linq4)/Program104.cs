3ba6dfe7e7208c03d625bf24b584a14b2d313ea7
Daniel Laptop <daniel.barczynski@microsoft.wsei.edu.pl>
2022-08-13 17:24:18 +0200

    update

--- Naukaaa104(linq4)/Program104.cs
+++ Naukaaa104(linq4)/Program104.cs
@@ -84,6 +84,34 @@ foreach (var item in emptyList)
     Console.WriteLine(item);
 }
 
+//-----------------------------------------------------------------------------------------
+Console.WriteLine("------------------------");
+
+// Creating a list of numbers
+List<int> numbers = new List<int> { 1, 2, 3, 4 };
+
+// Trying to append any value of the same type
+numbers.Append(5);
+
+// It doesn't work because the original list has not been changed
+Console.WriteLine(string.Join(", ", numbers));
+
+// It works now because we are using a changed copy of the original list
+Console.WriteLine(string.Join(", ", numbers.Append(5)));
+
+// If you prefer, you can create a new list explicitly
+List<int> newNumbers = numbers.Append(5).ToList();
+
+// And then write to the console output
+Console.WriteLine(string.Join(", ", newNumbers));
+
+// This code produces the following output:
+//
+// 1, 2, 3, 4
+// 1, 2, 3, 4, 5
+// 1, 2, 3, 4, 5
+
+
 class Employee 
 {
     public int EmployeeId { get; set; }
