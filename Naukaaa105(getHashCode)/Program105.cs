859cdb44e462d1458a518cba2ab3e4e5515d3681
Daniel Laptop <daniel.barczynski@microsoft.wsei.edu.pl>
2022-08-14 20:11:23 +0200

    update

---
+++ Naukaaa105(getHashCode)/Program105.cs
@@ -0,0 +1,49 @@
+﻿PointC pointC = new PointC()
+{
+    X = 100, Y = 200
+};
+
+PointC pointC2 = new PointC()
+{
+    X = 100, Y = 200
+};
+
+PointS pointS = new PointS()
+{
+    X = 100, Y = 200
+};
+
+PointS pointS2 = new PointS()
+{
+    X = 100, Y = 200
+};
+
+PointR pointR = new PointR()
+{
+    X = 100, Y = 200
+};
+
+PointR pointR2 = new PointR()
+{
+    X = 100, Y = 200
+};
+
+Console.WriteLine(pointC == pointC2); // compare references
+// Console.WriteLine(pointS == pointS2); error
+Console.WriteLine(pointR == pointR2); // compare values
+
+public class PointC
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+}
+public struct PointS
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+}
+public record PointR
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+}
