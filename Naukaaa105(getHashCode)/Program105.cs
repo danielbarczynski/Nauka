a99cfeb790d4a5d04d50b479693f88e0d4525f5b
Daniel Laptop <daniel.barczynski@microsoft.wsei.edu.pl>
2022-08-15 12:18:46 +0200

    update

--- Naukaaa105(getHashCode)/Program105.cs
+++ Naukaaa105(getHashCode)/Program105.cs
@@ -1,11 +1,13 @@
-﻿PointC pointC = new PointC()
+﻿using System.Collections.Generic;
+
+PointC pointC = new PointC()
 {
     X = 100, Y = 200
 };
 
 PointC pointC2 = new PointC()
 {
-    X = 100, Y = 200
+    X = 100, Y = 300
 };
 
 PointS pointS = new PointS()
@@ -38,13 +40,33 @@ Console.WriteLine(pointC.Equals(pointC2)); // false, after overriding true
 Console.WriteLine(pointS.Equals(pointS2)); // true, compares values
 Console.WriteLine(pointR.Equals(pointR2)); // true, compares values
 
+Console.WriteLine("-------------------");
+
+Console.WriteLine(pointC.GetHashCode() + " " + pointC2.GetHashCode);
+
+PointC pointC3 = new PointC()
+{
+    X = 100, Y = 300
+};
+
+Dictionary<PointC, string> dictionary = new Dictionary<PointC, string>();
+dictionary.Add(pointC, "c1");
+dictionary.Add(pointC2, "c2");
+
+if (dictionary.ContainsKey(pointC3)) // with gethascode it is much faster to look through dictionary
+{
+    Console.WriteLine(dictionary[pointC3]);
+}
+
+
 public class PointC : IEquatable<PointC>
 {
-    public int X { get; set; }
-    public int Y { get; set; }
+    public int X { get; init; }
+    public int Y { get; init; }
 
     public bool Equals(PointC other)
     {
+        Console.WriteLine("Calling Equals"); // equals is called as the final check
         return other != null && this.GetType() == other.GetType() && this.X == other.X && this.Y == other.Y; 
         //&& this != null is useless cuz instance of first object must be always set  
     }
@@ -57,8 +79,12 @@ public class PointC : IEquatable<PointC>
 
     public override int GetHashCode()
     {
+        Console.WriteLine("Calling GetHashCode");
+
         // return base.GetHashCode();
-        return X ^ Y;
+        // return X ^ Y; // xor | 1 & 1 = 0 | 0 & 0 = 0 | 1 & 0 = 1 | 0 & 1 = 1 |
+        // so if x = 100, y = 200 and x = 200, y = 100, it will be the same hashcode
+        return HashCode.Combine(X, Y); // best ay
     }
 }
 public struct PointS
