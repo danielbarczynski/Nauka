60f1c4d7357b5f6fd4475f97b65f9271a2d64df4
Daniel Barczyński <daniel.barczynski@onet.pl>
2022-12-03 20:04:00 +0100

    update

--- Naukaa68(yield)/Program68.cs
+++ Naukaa68(yield)/Program68.cs
@@ -18,12 +18,16 @@ IEnumerable<int> Integers()
 
 Consumer();
 
+//--------------------------------------------------------------------
+
 foreach (int i in ProduceEvenNumbers(9))
 {
     Console.Write(i);
     Console.Write(" ");
 }
 // Output: 0 2 4 6 8
+
+Console.WriteLine();
 
 IEnumerable<int> ProduceEvenNumbers(int upto)
 {
@@ -32,6 +36,8 @@ IEnumerable<int> ProduceEvenNumbers(int upto)
         yield return i; // without yield error "not all code returns a value"
     }
 }
+
+//--------------------------------------------------------------------
 
 Console.WriteLine(string.Join(" ", TakeWhilePositive(new[] { 2, 3, 4, 5, -1, 3, 4 })));
 // Output: 2 3 4 5
@@ -49,7 +55,78 @@ IEnumerable<int> TakeWhilePositive(IEnumerable<int> numbers)
         }
         else
         {
-            yield break;
+            yield break; // or just break
         }
+    }
+}
+
+var point = new Point(1, 2, 3);
+
+foreach (int coordinate in point)
+{
+    Console.Write(coordinate);
+    Console.Write(" ");
+}
+
+//--------------------------------------------------------------------
+
+var numbers = ProduceEvenNumbers2(5);
+Console.WriteLine("Caller: about to iterate.");
+
+foreach (int i in numbers) // it comes back everytime
+{
+    Console.WriteLine($"Caller: {i}"); 
+}
+
+Console.WriteLine("new numbers start");
+Console.WriteLine("new numbers end");
+
+
+object h = hey();
+Console.WriteLine(h);
+
+Console.WriteLine("start yielding");
+// Console.WriteLine(returnYield()); // wont work, must be iteration through foreach for instance
+
+foreach (var item in returnYield())
+{
+    Console.WriteLine(item);
+}
+
+Console.WriteLine("stop yielding");
+
+string hey() => "hey";
+
+IEnumerable<string> returnYield() //* yield can be only IEnumerable or IEnumerator (in class) type 
+{
+    yield return "my";
+    yield return "name";
+    yield return "is";
+}
+
+IEnumerable<int> ProduceEvenNumbers2(int upto)
+{
+    Console.WriteLine("Iterator: start.");
+
+    for (int i = 0; i <= upto; i += 2)
+    {
+        Console.WriteLine($"Iterator: about to yield {i}");
+        yield return i; // it comes back everytime
+        Console.WriteLine($"Iterator: yielded {i}");
+    }
+
+    Console.WriteLine("Iterator: end.");
+}
+
+
+
+// yield with GetEnumerator
+public readonly record struct Point(int X, int Y, int Z) // can't do it with the class
+{
+    public IEnumerator<int> GetEnumerator()
+    {
+        yield return X;
+        yield return Y;
+        yield return Z;
     }
 }
