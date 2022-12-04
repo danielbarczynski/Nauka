8ea99f6e58a4f70c6b3a2b9b37b27014d0d8fcc0
Daniel PC <daniel.barczynski@onet.pl>
2022-12-04 18:35:04 +0100

    update

---
+++ Naukaaa114(EventHandler2)/Program114.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+Console.WriteLine("Press A");
+var key = Console.ReadLine(); 
+
+static void KeyPressed() 
+{
+    if (key == 'a')
+        MyClass.OnClick();
+}
+
+public class MyClass
+{
+    public event EventHandler ClickEvent;
+    public static void OnClick()
+    {
+        ClickEvent(this, EventArgs.Empty);
+    }
+}
