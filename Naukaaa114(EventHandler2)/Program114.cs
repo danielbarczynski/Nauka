861bc75d70bf3e315d1d7aabc70a92e2cbba41a1
Daniel PC <daniel.barczynski@onet.pl>
2022-12-04 19:39:25 +0100

    update

--- Naukaaa114(EventHandler2)/Program114.cs
+++ Naukaaa114(EventHandler2)/Program114.cs
@@ -2,6 +2,7 @@
 
 Console.WriteLine("Press A");
 var key = Console.ReadLine(); 
+
 if (key == "a")
     KeyPressed();
 
@@ -10,17 +11,26 @@ static void KeyPressed()
     Button button = new();    
     button.ClickEvent += (sender, e) =>
     {
-        Console.WriteLine("Button clicked");
+        e.Name = "Ian";
+        Console.WriteLine($"Button clicked {e.Name}");
     };
-    
+
     button.OnClick();
 }
 
 public class Button
 {
-    public event EventHandler ClickEvent;
+    public event EventHandler<MyArgs> ClickEvent; // without <MyArgs> it won't see a Name prop
     public void OnClick()
     {
-        ClickEvent(this, EventArgs.Empty);
+        MyArgs myArgs = new();
+
+        if (ClickEvent != null)
+            ClickEvent.Invoke(this, myArgs);
     }
+}
+
+public class MyArgs : EventArgs
+{
+    public string Name { get; set; } = "Daniel";
 }
