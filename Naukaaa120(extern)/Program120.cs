0e831853f99f3fe9bf154ad7c3091ecbdf56ebcb
Daniel PC <daniel.barczynski@onet.pl>
2022-12-23 20:20:24 +0100

    up

---
+++ Naukaaa120(extern)/Program120.cs
@@ -0,0 +1,8 @@
+﻿using System.Runtime.InteropServices;
+
+// using external code from another language or from windows library in this case
+[DllImport("User32.dll")]
+static extern int MessageBox(int h, string m, string c, int type);
+
+var result = MessageBox(0, "trojan.exe", "virus alert", 0);
+Console.WriteLine(result);
