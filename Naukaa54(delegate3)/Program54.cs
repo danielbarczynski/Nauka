47eb8663960963e84de3c266dfafd1551a1ac002
Daniel PC <daniel.barczynski@onet.pl>
2023-01-04 13:06:55 +0100

    up

--- Naukaa54(delegate3)/Program54.cs
+++ Naukaa54(delegate3)/Program54.cs
@@ -1,11 +1,13 @@
-﻿MyDel myDel = new MyDel(SayHello); // we can pass a function
-MyDel myDel2 = x => x + " no hello"; // we can assing anonymous method/function
+﻿MyDel myDel = new MyDel(SayHello); // we can pass a function in constrctor
+MyDel myDel2 = SayHello; // or just assign a function
+MyDel myDel3 = x => x + " no hello"; // we can assing anonymous method/function
 
 Console.WriteLine(SayHello("Daniel"));
 Console.WriteLine(SayHello2(x => x));
 Console.WriteLine(SayHello2(x => x + " and bye"));
 Console.WriteLine(myDel("Kayla"));
 Console.WriteLine(myDel2("Kayla"));
+Console.WriteLine(myDel3("Kayla"));
 
 string SayHello(string name) => $"Hello {name}";
 string SayHello2(MyDel myDel) => myDel(SayHello("Bryan"));
